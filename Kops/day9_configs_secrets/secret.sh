6d72f20408234cad683b7bef0e17a0956a6cf7c1
Rajat <saxenarajat499@gmail.com>
2024-10-26 21:18:53 +0530

    end

--- Kops/day9_configs_secrets/secret.sh
+++ Kops/day9_configs_secrets/secret.sh
@@ -1,9 +1,9 @@
 # when we pull the images from the private repositry, we can use the secrets using the regsitry
 
-kubectl create secret docker-registry docker-psd \
---docker-server=docker.io --docker-username=saxenarajat499 \
---docker-password=dckr_pat_4P9BiLeDvBsBn_tlIbu2RK8ecPM \
---docker-email=H8n0M@example.com
+# kubectl create secret docker-registry docker-psd \
+# --docker-server=docker.io --docker-username=saxenarajat499 \
+# --docker-password=dckr_pat_4P9BiLeDvBsBn_tlIbu2RK8ecPM \
+# --docker-email=H8n0M@example.com
 
 
 ---
